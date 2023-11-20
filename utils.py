bfcc99fb9ce3442ffed8cfe604808b4ef9b1ef01
GionniGi <gcrapuzzi02@gmail.com>
2023-11-20 10:47:43 +0100

    Refactor password, email, and username validation functions

--- utils.py
+++ utils.py
@@ -1,13 +1,10 @@
 import datetime
 import re
 import bcrypt
-import random
-import string
-import base64
 from flask_mail import Mail, Message
 
 # Function to validate password
-def validate_password(password):
+def validatePassword(password):
     if len(password) < 8:
         return False
     elif not any(char.isdigit() for char in password):
@@ -20,14 +17,14 @@ def validate_password(password):
         return True
     
 # Function to validate email
-def validate_email(email):
+def validateEmail(email):
     if len(email) > 7:
         if re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email) != None:
             return True
     return False
 
 # Function to validate username
-def validate_username(username):
+def validateUsername(username):
     if len(username) < 3:
         return False
     elif not any(char.isalpha() for char in username):
@@ -38,14 +35,13 @@ def validate_username(username):
         return True
     
 # Function to hash password
-def hash_password(password):
+def hashPassword(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 # Function to check password
-def check_password(password, hashed):
+def checkPassword(password, hashed):
     return bcrypt.checkpw(password.encode('utf-8'), hashed)
 
 # Function to get ip address
-def get_ip(request):
+def getIp(request):
     return request.headers.get('X-Forwarded-For', request.remote_addr)
-

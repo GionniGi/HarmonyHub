6720216ff0e56525ca4122a2fc872a5fa687bda6
GionniGi <gcrapuzzi02@gmail.com>
2023-11-21 12:30:10 +0100

    Refactor signup route parameters

--- models/user/routes.py
+++ models/user/routes.py
@@ -3,26 +3,26 @@ from app import app
 from utils import getIp
 from models.user.user import User
 
-# Add a signup route
 @app.route('/signup', methods=['POST'])
 def signup():
-     try:
-          # Get request data
-          username = request.json['username']
-          email = request.json['email']
-          password = request.json['password']
-          confirmPassword = request.json['confirmPassword']
-          firstName = request.json['firstName']
-          lastName = request.json['lastName']
-          birthDate = request.json['birthDate']
-          description = request.json['description']
-          extroversion = request.json['extroversion']
-          friendliness = request.json['friendliness']
-          emotionalStability = request.json['emotionalStability']
-          openness = request.json['openness']
-          conscientiousness = request.json['conscientiousness']
-          ipAddress = getIp(request)
-          user = User()
-          return user.signup(username, email, password, confirmPassword, firstName, lastName, birthDate, description, extroversion, friendliness, emotionalStability, openness, conscientiousness, ipAddress), render_template('login.html')
-     except Exception as e:
-          return str(e), 400
+    # Add a signup route
+    try:
+        # Get request data
+        username = request.json['username']
+        email = request.json['email']
+        password = request.json['password']
+        confirm_password = request.json['confirmPassword']
+        first_name = request.json['firstName']
+        last_name = request.json['lastName']
+        birth_date = request.json['birthDate']
+        description = request.json['description']
+        extroversion = request.json['extroversion']
+        friendliness = request.json['friendliness']
+        emotional_stability = request.json['emotionalStability']
+        openness = request.json['openness']
+        conscientiousness = request.json['conscientiousness']
+        ip_address = getIp(request)          
+        user = User()
+        return user.signup(username, email, password, confirm_password, first_name, last_name, birth_date, description, extroversion, friendliness, emotional_stability, openness, conscientiousness, ip_address), render_template('login.html')
+    except Exception as e:
+        return str(e), 400

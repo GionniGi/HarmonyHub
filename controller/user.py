523d7fba5b68a4c8cf0b03eda432f809f7056112
GionniGi <gcrapuzzi02@gmail.com>
2023-11-25 16:17:17 +0100

    Refactor signup functionality

--- controller/user.py
+++ controller/user.py
@@ -4,23 +4,30 @@ from models.user import User
 from app import users
 from flask import request, jsonify
 
+# Validate signup data
+def validate_signup_data(username, email, password, confirm_password):
 
-def signup(username, email, password, confirm_password, first_name, last_name, birth_date, description, extroversion, friendliness, conscientiousness, openness, emotional_stability, ip_address):
-    # Validate input
+    # Check if all fields are filled
     if password != confirm_password:
-        return {"message": "Passwords do not match."}
+        raise ValueError("Passwords do not match.")
     if not validate_password(password):
-        return {"message": "Password must be at least 8 characters long and contain at least one digit, one uppercase letter, and one lowercase letter."}
+        raise ValueError("Password must be at least 8 characters long...")
     if not validate_email(email):
-        return {"message": "Email is not valid."}
+        raise ValueError("Email is not valid.")
     if not validate_username(username):
-        return {"message": "Username must be at least 3 characters long."}
+        raise ValueError("Username must be at least 3 characters long.")
     
-    # Check if user already exists
+    # Check if username or email already exists
     if users.find_one({'username': username}):
-        return {"message": "Username already taken."}
+        raise ValueError("Username already taken.")
     if users.find_one({'email': email}):
-        return {"message": "Email already taken."}
+        raise ValueError("Email already taken.")
+
+# Signup user
+def signup(username, email, password, confirm_password, first_name, last_name, birth_date, description, extroversion, friendliness, conscientiousness, openness, emotional_stability, ip_address):
+    
+    # Validate data
+    validate_signup_data(username, email, password, confirm_password)
 
     # Create user and insert into database
     new_user = User(username, email, hash_password(password), first_name, last_name, birth_date, description, extroversion, friendliness, conscientiousness, openness, emotional_stability)
@@ -30,6 +37,7 @@ def signup(username, email, password, confirm_password, first_name, last_name, b
     users.insert_one(user_object)
     return {"message": "User created successfully."}
 
+# Post method for signup
 def signup_post():
     data = request.json
     if not data:
@@ -71,4 +79,3 @@ def signup_post():
             ip_address
         )
     return jsonify(response), 200
-
--- routes/user.py
+++ routes/user.py
@@ -1,28 +1,21 @@
-from flask import request, Blueprint, jsonify, render_template
-from controllers.user import signup_post
+from flask import Blueprint, jsonify, render_template
+from controller.user import signup_post
 
 user_blueprint = Blueprint('user', __name__)
 
-@user_blueprint.route('/signup/', methods=['GET', 'POST'])
-def signup():
-    """
-    Handle user signup.
+@user_blueprint.route('/signup/', methods=['GET'])
+# Show signup page
+def show_signup():
+    return render_template('signup.html')
 
-    GET: Return signup page.
-    POST: Signup user.
-    """
-    if request.method == 'GET':
-        return render_template('signup.html')
+@user_blueprint.route('/signup/', methods=['POST'])
+# Signup user
+def process_signup():
     try:
-        user_post()
+        return signup_post()
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
     except KeyError as e:
         return jsonify({"error": str(e)}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 400
-
-@user_blueprint.route('/login/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
